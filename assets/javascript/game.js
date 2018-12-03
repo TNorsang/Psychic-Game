2dd758524a85a4c9b475a2f0908d5221c408b6b1
git config --global user.name TNorsang <thenorsang@gmail.com>
2018-12-03 11:21:24 -0500

    added index.html texts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,28 +2,32 @@ var letters='abcdefghijklmnopqrstuvwxyz';
 var computerLetters=letters.split('');
 var wins = 0;
 var losses = 0;
+var correctGuess = false;
 
-var whatLetter = document.getElementById("what-letter");
+
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
 var guessesLeft = document.getElementById("guesses-left");
 var guessesSoFar = document.getElementById("guesses-so-far");
 
+// for (i=0; i<10;)
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
 computerLetters[Math.floor(Math.random() * computerLetters.length)];
 }
 
-if ((userGuess === "") || (userGuess === "") || (userGuess === "")) {
+userGuess.onkeyup
 
-    if ((userGuess === "" && computerGuess === "") ||
-      (userGuess === "" && computerGuess === "") || 
-      (userGuess === "" && computerGuess === "")) {
-      wins++;
-    } else if (userGuess === computerGuess) {
-      ties++;
-    } else {
-      losses++;
-    }
+// if ((userGuess === "") || (userGuess === "") || (userGuess === "")) {
+
+//     if ((userGuess === "" && computerGuess === "") ||
+//       (userGuess === "" && computerGuess === "") || 
+//       (userGuess === "" && computerGuess === "")) {
+//       wins++;
+//     } else if (userGuess === computerGuess) {
+//       ties++;
+//     } else {
+//       losses++;
+//     }
 
